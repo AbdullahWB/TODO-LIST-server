22667efc3cb66a702928bf8ebc1b6bfef7ad534b
AbdullahWB <abdullah917828@gmail.com>
2023-06-01 21:22:24 +0600

    make post and delet item

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.luk9jtm.mongodb.net/?retryWrites=true&w=majority`;;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,6 +33,20 @@ async function run() {
         app.get('/list', async (req, res) => {
             const data = dataCollection.find();
             const result = await data.toArray();
+            res.send(result)
+        })
+
+        app.post('/list', async (req, res) => {
+            const newData = req.body;
+            console.log(newData);
+            const result = await dataCollection.insertOne(newData);
+            res.send(result)
+        })
+
+        app.delete('/list/:id', async (req, res) => { 
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await dataCollection.findOne(query);
             res.send(result)
         })
 
