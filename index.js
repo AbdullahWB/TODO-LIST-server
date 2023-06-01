dee682f4dc409adccbbe0f770f3d0fa6cfcb5924
AbdullahWB <abdullah917828@gmail.com>
2023-06-01 22:10:45 +0600

    make details route

--- index.js
+++ index.js
@@ -43,11 +43,18 @@ async function run() {
             res.send(result)
         })
 
-        app.delete('/list/:id', async (req, res) => { 
+        app.get('/list/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await dataCollection.findOne(query);
             res.send(result)
+        })
+
+        app.delete('/list/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await dataCollection.deleteOne(query)
+            res.send(result);
         })
 
 
